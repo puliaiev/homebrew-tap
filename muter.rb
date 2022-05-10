6171c759099dd2369b9d6f22b049d19d82eb75b6
Serhii Puliaiev <serj1903@gmail.com>
2022-05-10 17:14:34 +0200

    Add Muter

---
+++ muter.rb
@@ -0,0 +1,11 @@
+cask "muter" do
+    version "0.1.0"
+    sha256 "7696f5b55539ac6b71bb71e9a81ed876aaecadb130a854a48f2956e6d3193f99"
+
+    url "https://github.com/puliaiev/Muter/releases/download/#{version}/Muter.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    name "Muter"
+    desc "App to mute your mic"
+    homepage "https://github.com/puliaiev/Muter"
+
+    app "Muter.app"
+end
